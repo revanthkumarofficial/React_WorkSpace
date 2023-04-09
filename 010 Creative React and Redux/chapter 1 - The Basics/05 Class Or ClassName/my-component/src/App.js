57737f9251816ae53e87f7d1923e2ebfc8b5ae81
revanthkumarofficial <75003330+revanthkumarofficial@users.noreply.github.com>
2023-04-09 13:48:09 +0530

    eight commit

---
+++ 010 Creative React and Redux/chapter 1 - The Basics/05 Class Or ClassName/my-component/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TestComponent from "./TestComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <TestComponent />
+    </div>
+  );
+}
+
+export default App;
